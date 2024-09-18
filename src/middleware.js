0a5b9336c1640c24a95a1b6d0c54a993489bc13b
xingwanyue <xwymail@qq.com>
2024-09-18 11:06:05 +0800

    middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,8 @@
 import { withLocales } from "nextra/locales";
+import { NextResponse } from "next/server";
 
 export const middleware = withLocales((request) => {
   // Your middleware code...
+  const response = NextResponse.next();
+  return response;
 });
